57b8979107090fb6a7202c8d509c7a717b42aa38
dovanthanhgm <149997073+dovanthanhgm@users.noreply.github.com>
2023-12-18 10:02:22 +0000

    up

--- core/models.py
+++ core/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+
 class Product(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
